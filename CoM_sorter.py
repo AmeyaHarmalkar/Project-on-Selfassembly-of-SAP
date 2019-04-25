b124e134bc92c60b7ac0a6dedc13a84bb53670ab
Ameya <aharmal1@GRAY-mac06.graylab.jhu.edu>
2019-04-25 15:27:24 -0400

    Final code for the sorter

--- CoM_sorter.py
+++ CoM_sorter.py
@@ -13,6 +13,8 @@ from CoM_filter import *
 
 directory = '/Users/aharmal1/Ameya/Spring_2019/Project/prediction/output'
 
+output = open("output.txt", "w")
+
 for file in os.listdir(directory) :
 	
 	if file.endswith(".pdb") :
@@ -24,13 +26,13 @@ for file in os.listdir(directory) :
 		d3 = centre_of_mass_calc(pose, 1, 89)
 		d4 = centre_of_mass_calc(pose, 335, 440)
 
-		print(d1)
-		print(d2)
-		print(d3)
-		print(d4)
+		is_planar = planarity_checker(d1,d2,d3,d4)
+
+		if is_planar:
+			output.write(file+"\n")
+
 		
-		break
-		#continue
+		continue
 	
 	#else :
 		#continue
